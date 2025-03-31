ba44c001e23a057f10c5fd3bf53fa183e07f1db3
Immaculate <101836390+LordImmaculate@users.noreply.github.com>
2025-03-31 16:07:05 +0200

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import { title } from "@/components/primitives";
+
+export default function Home() {
+  return (
+    <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
+      <div className="inline-block max-w-xl text-center justify-center">
+        <span className={title()}>Welcome to your new HeroUI project</span>
+      </div>
+    </section>
+  );
+}
---
+++ config/site.ts
@@ -0,0 +1,21 @@
+export type SiteConfig = typeof siteConfig;
+
+export const siteConfig = {
+  name: "Hero UI Template",
+  description: "A custom HeroUI Template made by LordImmaculate",
+  navItems: [
+    {
+      label: "Home",
+      href: "/"
+    }
+  ],
+  navMenuItems: [
+    {
+      label: "Profile",
+      href: "/home"
+    }
+  ],
+  links: {
+    github: "https://github.com/lordimmaculate"
+  }
+};
